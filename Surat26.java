9e14a79a0cd472baa95df09861d0b6c5e0fec0a7
naraafch <rochiszigmakinara@gmail.com>
2026-04-24 12:05:21 +0700

    tugas 1

---
+++ Surat26.java
@@ -0,0 +1,18 @@
+public class Surat26 {
+    String idSurat, namaMahasiswa, kelas;
+    char jenisIzin;
+    int durasi;
+
+    Surat26() {
+
+    }
+
+    Surat26(String idSurat, String namaMahasiswa, String kelas, char jenisIzin, int durasi) {
+        this.idSurat= idSurat;
+        this.namaMahasiswa= namaMahasiswa;
+        this.kelas= kelas;
+        this.jenisIzin= jenisIzin;
+        this.durasi= durasi;
+    }
+    
+}
