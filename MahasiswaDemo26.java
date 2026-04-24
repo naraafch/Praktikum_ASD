fb192a64eaad6360f3d34ca7863318c764cfc5b0
naraafch <rochiszigmakinara@gmail.com>
2026-04-24 10:38:24 +0700

    percobaan 2

--- MahasiswaDemo26.java
+++ MahasiswaDemo26.java
@@ -38,6 +38,8 @@ public class MahasiswaDemo26 {
                         int nilai= sc.nextInt();
                         dinilai.tugasDinilai(nilai);
                         System.out.printf("Nilai tugas %s adalah %d\n", dinilai.nama, nilai);
+                        String biner= stack.konversiDesimalKeBiner(nilai);
+                        System.out.println("Nilai biner tugas: "+biner);
                     }
                     break;
 
