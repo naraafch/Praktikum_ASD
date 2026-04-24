2f2571c2400dd1820cd5d9ced3eea091a4cfd67f
naraafch <rochiszigmakinara@gmail.com>
2026-04-24 09:12:15 +0700

    Mahasiswa26

---
+++ Mahasiswa26.java
@@ -0,0 +1,21 @@
+public class Mahasiswa26 {
+    String nim, nama, kelas;
+    int nilai;
+
+    Mahasiswa26() {
+
+    }
+
+    Mahasiswa26(String nim, String nama, String kelas) {
+        this.nim= nim;
+        this.nama= nama;
+        this.kelas= kelas;
+        nilai= -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai= nilai;
+    }
+    
+    
+}
